7fd6db3e396cf9412d342fdb1f62e35edc6412ad
behradrez <r.behrad121@gmail.com>
2024-02-14 20:56:26 -0500

    Initial commit

---
+++ exam-schedules/src/main/java/com/rez/bear/examschedules/ExamSchedulesApplication.java
@@ -0,0 +1,13 @@
+package com.rez.bear.examschedules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamSchedulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamSchedulesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rez/bear/examschedules/ExamSchedulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rez.bear.examschedules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamSchedulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
